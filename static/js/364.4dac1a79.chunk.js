b84456cd5de33cec6fef6393b9909c6a5d12d215
giorgiospedaledev <giorgiospedaledev@gmail.com>
2023-05-22 16:38:14 +0200

    Updates

---
+++ static/js/364.4dac1a79.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrancescapollono=self.webpackChunkfrancescapollono||[]).push([[364],{9364:function(n,e,r){r.r(e),r.d(e,{default:function(){return l}});var c=r(2791),t=r(3811),a=r(184),s=function(){return(0,a.jsx)(t.YI,{children:(0,a.jsx)(t.WD,{name:"App",children:(0,a.jsx)(t.wA,{name:"Loader",children:(0,a.jsx)(t.$e,{children:(0,a.jsx)(u,{})})})})})};function u(){return(0,a.jsx)(c.Fragment,{children:"Loading..."})}var l=function(){return(0,a.jsx)(t.Vt,{palette:(0,a.jsx)(s,{})})}}}]);
+//# sourceMappingURL=364.4dac1a79.chunk.js.map
